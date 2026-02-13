cec0998b7fc7a8bc60e91c914ef9508e7293d515
Megan Clapinski <142531799+meganclapinski25@users.noreply.github.com>
2026-02-12 16:26:49 -0800

    finished signpost 2

--- features/animals/animalsSlice.js
+++ features/animals/animalsSlice.js
@@ -41,9 +41,13 @@ const animalsSlice = createSlice({
       })
       .addCase(fetchAnimals.fulfilled, (state, action) => {
         // TODO (Signpost 2): set status to 'succeeded' and set animals = action.payload
+        state.status = 'succeeded'
+        animals = action.payload
       })
       .addCase(fetchAnimals.rejected, (state, action) => {
         // TODO (Signpost 2): set status to 'failed' and set error = action.error.message
+        state.status = 'failed'
+        error = action.error.message
       });
   },
 });
